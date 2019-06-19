25b79376fb927efc830194a5c55a3014dbb71152
jannajan <janj58102@gmail.com>
2019-06-18 23:52:59 -0500

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Joanna Jan on 6/18/19.
+//  Copyright © 2019 Joanna Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
